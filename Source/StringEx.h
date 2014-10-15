4718bca961fba12ab75c36708502c192ef999173
Stanislav Artemkin <artemkin@gmail.com>
2014-10-15 23:58:05 +0400

    Initial XCode project

---
+++ Source/StringEx.h
@@ -0,0 +1,18 @@
+//
+//  StringEx.h
+//  StringEx
+//
+//  Copyright (c) 2014 Stanislav Artemkin <artemkin@gmail.com>. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringEx.
+FOUNDATION_EXPORT double StringExVersionNumber;
+
+//! Project version string for StringEx.
+FOUNDATION_EXPORT const unsigned char StringExVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringEx/PublicHeader.h>
+
+
---
+++ Source/StringEx.swift
@@ -0,0 +1,8 @@
+//
+//  StringEx.swift
+//  StringEx
+//
+//  Copyright (c) 2014 Stanislav Artemkin <artemkin@gmail.com>. All rights reserved.
+//
+
+import Foundation
