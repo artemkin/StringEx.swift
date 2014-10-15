565c597192e01e83506d0227b666794876cea7f9
Stanislav Artemkin <artemkin@gmail.com>
2014-10-16 00:36:47 +0400

    Added toInt(), toUInt() and toDouble() + unit tests

--- Source/StringEx.swift
+++ Source/StringEx.swift
@@ -6,3 +6,144 @@
 //
 
 import Foundation
+
+
+extension String {
+
+    /// If the string represents an unsigned integer that fits into a specified
+    /// unsigned integer type, returns the corresponding integer.
+    /// This accepts strings that match the regular expression "[0-9]+" only.
+    public func toUInt()   -> UInt?   { return toUnsignedT() }
+    public func toUInt8()  -> UInt8?  { return toUnsignedT() }
+    public func toUInt16() -> UInt16? { return toUnsignedT() }
+    public func toUInt32() -> UInt32? { return toUnsignedT() }
+    public func toUInt64() -> UInt64? { return toUnsignedT() }
+
+    /// If the string represents an integer that fits into a specified integer type,
+    /// returns the corresponding integer.  This accepts strings that match the regular
+    /// expression "[-+]?[0-9]+" only.
+    public func toInt8()   -> Int8?   { return toSignedT() }
+    public func toInt16()  -> Int16?  { return toSignedT() }
+    public func toInt32()  -> Int32?  { return toSignedT() }
+    public func toInt64()  -> Int64?  { return toSignedT() }
+
+    /// If the string represents a floating point number, returns the corresponding
+    /// number as double. It is more strict than C strtod: whitespace characters
+    /// and empty strings aren't allowed.
+    public func toDouble() -> Double? { return toDoubleImpl() }
+}
+
+protocol UnsignedIntegerTypeEx: UnsignedIntegerType {
+    class var max: Self { get }
+    init(_: UInt32)
+}
+extension UInt:   UnsignedIntegerTypeEx {}
+extension UInt8:  UnsignedIntegerTypeEx {}
+extension UInt16: UnsignedIntegerTypeEx {}
+extension UInt32: UnsignedIntegerTypeEx {}
+extension UInt64: UnsignedIntegerTypeEx {}
+
+extension String {
+    func toUnsignedT<T: UnsignedIntegerTypeEx>() -> T? {
+        if self.isEmpty {
+            return nil
+        }
+
+        let maxDiv10 = T.max / 10
+
+        var x: T = 0
+        for ch in self.unicodeScalars {
+            let value = ch.value
+            if value < 0x30 || value > 0x39 || x > maxDiv10 {
+                return nil
+            }
+            x *= 10
+            let num = T(value - 0x30)
+            if x > (T.max - num) {
+                return nil
+            }
+            x += num
+        }
+        return x
+    }
+}
+
+
+protocol SignedIntegerTypeEx: SignedIntegerType {
+    class var max: Self { get }
+    class var min: Self { get }
+    init(_: UInt32)
+}
+extension Int8:  SignedIntegerTypeEx {}
+extension Int16: SignedIntegerTypeEx {}
+extension Int32: SignedIntegerTypeEx {}
+extension Int64: SignedIntegerTypeEx {}
+
+extension String {
+    func toSignedT<T: SignedIntegerTypeEx>() -> T? {
+        if self.isEmpty {
+            return nil
+        }
+
+        var characters = self.unicodeScalars
+        let firstVal = first(characters)!.value
+        let isMinus = firstVal == 0x2D
+        if isMinus || firstVal == 0x2B /* + */ {
+            characters = dropFirst(characters)
+            if characters.startIndex == characters.endIndex {
+                return nil
+            }
+        }
+
+        if isMinus {
+            let minDiv10 = T.min / 10
+            var x: T = 0
+
+            for ch in characters {
+                let value = ch.value
+                if value < 0x30 || value > 0x39 || x < minDiv10 {
+                    return nil
+                }
+                x *= 10
+                let num = T(value - 0x30)
+                if x < (T.min + num) {
+                    return nil
+                }
+                x -= num
+            }
+            return x
+        }
+
+        let maxDiv10 = T.max / 10
+        var x: T = 0
+
+        for ch in characters {
+            let value = ch.value
+            if value < 0x30 || value > 0x39 || x > maxDiv10 {
+                return nil
+            }
+            x *= 10
+            let num = T(value - 0x30)
+            if x > (T.max - num) {
+                return nil
+            }
+            x += num
+        }
+        return x
+    }
+}
+
+extension String {
+
+    func toDoubleImpl() -> Double? {
+        if self.isEmpty || self.hasPrefix(" ") {
+            return nil
+        }
+
+        return self.withCString() { p -> Double? in
+            var end: UnsafeMutablePointer<Int8> = nil
+            let result = strtod(p, &end)
+            return end.memory != 0 ? nil : result
+        }
+    }
+}
--- Tests/StringExTests.swift
+++ Tests/StringExTests.swift
@@ -6,29 +6,79 @@
 //
 
 import XCTest
+import StringEx
 
 class StringExTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_stringToUInt() {
+        XCTAssert("".toUInt() == nil)
+        XCTAssert(" ".toUInt() == nil)
+        XCTAssert(" 1".toUInt() == nil)
+        XCTAssert("1 ".toUInt() == nil)
+        XCTAssert("-1".toUInt() == nil)
+        XCTAssert("0".toUInt() == 0)
+        XCTAssert("11".toUInt() == 11)
+        XCTAssert("12345".toUInt() == 12345)
+        XCTAssert(String(UInt.max).toUInt() == UInt.max)
+        XCTAssert(String(UInt.min).toUInt() == UInt.min)
+        XCTAssert("18446744073709551620".toUInt() == nil) // 18446744073709551620 / 10 * 10 > UInt.max
+        XCTAssert("18446744073709551616".toUInt() == nil) // UInt.max + 1
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func test_stringToInt() {
+        XCTAssert("".toInt8() == nil)
+        XCTAssert(" ".toInt8() == nil)
+        XCTAssert("-".toInt8() == nil)
+        XCTAssert("+".toInt8() == nil)
+        XCTAssert(" 1".toInt8() == nil)
+        XCTAssert("1 ".toInt8() == nil)
+        XCTAssert("-1".toInt8() == -1)
+        XCTAssert("+1".toInt8() == 1)
+        XCTAssert("0".toInt8() == 0)
+        XCTAssert("11".toInt8() == 11)
+        XCTAssert("11.".toInt8() == nil)
+        XCTAssert("123".toInt8() == 123)
+        XCTAssert("-123".toInt8() == -123)
+        XCTAssert("-130".toInt8() == nil)
+        XCTAssert("130".toInt8() == nil)
+        XCTAssert(String(Int8.max).toInt8() == Int8.max)
+        XCTAssert(String(Int8.min).toInt8() == Int8.min)
+        XCTAssert("0000000000000000123".toInt8() == 123)
+        XCTAssert("+0000000000000000123".toInt8() == 123)
+        XCTAssert("-0000000000000000123".toInt8() == -123)
+        XCTAssert("--000000000000000123".toInt8() == nil)
+        XCTAssert("0-000000000000000123".toInt8() == nil)
+        XCTAssert("+-000000000000000123".toInt8() == nil)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func test_stringToDouble() {
+        XCTAssert("".toDouble() == nil)
+        XCTAssert(" ".toDouble() == nil)
+        XCTAssert("-".toDouble() == nil)
+        XCTAssert(".".toDouble() == nil)
+        XCTAssert("e".toDouble() == nil)
+        XCTAssert("+".toDouble() == nil)
+        XCTAssert(" 1".toDouble() == nil)
+        XCTAssert("1 ".toDouble() == nil)
+        XCTAssert(".1".toDouble() == 0.1)
+        XCTAssert(" .1".toDouble() == nil)
+        XCTAssert(".1 ".toDouble() == nil)
+        XCTAssert("-1".toDouble() == -1)
+        XCTAssert("+1".toDouble() == 1)
+        XCTAssert("0".toDouble() == 0)
+        XCTAssert("11".toDouble() == 11)
+        XCTAssert("11.".toDouble() == 11)
+        XCTAssert("123".toDouble() == 123)
+        XCTAssert("-123".toDouble() == -123)
+        XCTAssert("test-123".toDouble() == nil)
+        XCTAssert("1 2 3".toDouble() == nil)
+        XCTAssert("12e-3".toDouble() == 12e-3)
+        XCTAssert("12 e-3".toDouble() == nil)
+        XCTAssert("123.45678".toDouble() == 123.45678)
+        XCTAssert("-123.45678".toDouble() == -123.45678)
+        XCTAssert("0.1".toDouble() == 0.1)
+        XCTAssert("0.2".toDouble() == 0.2)
+        XCTAssert("0.3".toDouble() == 0.3)
+        XCTAssert("1.23456789012345e36".toDouble() == 1.23456789012345e36)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
